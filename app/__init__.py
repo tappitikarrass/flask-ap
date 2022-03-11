58dd29f979086174071aa6704030e2ba36c5116e
Andrii Lytvyn <lytvyn349@gmail.com>
2022-03-12 00:29:13 +0200

    Update curl script and requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import (Flask, request, jsonify)
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "super-secret"
@@ -12,8 +13,18 @@ jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
-SWAGGER_URL = "/api/docs"
+SWAGGER_URL = "/apidocs"
+API_URL = "/static/swagger.yml"
+
+bp_swagger_ui = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Test application"
+    }
+)
 
 from .user import bp_user
 
+app.register_blueprint(bp_swagger_ui)
 app.register_blueprint(bp_user)
